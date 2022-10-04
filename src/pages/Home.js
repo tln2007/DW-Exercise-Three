516efb77e69a7348ba9256711e9070cad538e065
Tasnim Nahar <tln2007@nyu.edu>
2022-10-04 09:06:45 -0400

    add packages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { WEATHER_APP_API_KEY } from "../API_KEYS";
 
 function Home() {
-    return (<div>
-        <h1>Weather App</h1>
-    </div>)
+    return (
+        <div>
+            <h1>Weather App</h1>
+        </div>
+    ); 
 }
 
 export default Home;
